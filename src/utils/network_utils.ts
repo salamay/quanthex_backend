c3dad25b54e3d905e95989d379cecc6c7aaccd4d
Salamay <ayotundesalam16@gmail.com>
2026-01-12 02:57:48 +0100

    Added rpc's

--- src/utils/network_utils.ts
+++ src/utils/network_utils.ts
@@ -1,6 +1,8 @@
 export class NetworkUtils {
     public static rpcs: {[chainId: number]: string}={
-        137:"https://go.getblock.us/6fca04d1f8204303bda2c7c8072cbd4a"
+        1:"https://go.getblock.io/8e30f8a3d9314d4785167d79abdf164c",
+        56:"https://go.getblock.io/119fad79ee5d4ffda4c3de8fdc77618e",
+        137:"https://go.getblock.io/5fa1b57ec3724b18944c5c10006d95ef"
     }
 
     public static getRpc(chainId: number): string {
