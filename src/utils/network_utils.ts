51c1d13a5f2a2954707668851fac7f7c663df055
Salamay <ayotundesalam16@gmail.com>
2026-02-10 01:44:18 +0100

    Changed rpc

--- src/utils/network_utils.ts
+++ src/utils/network_utils.ts
@@ -1,8 +1,8 @@
 export class NetworkUtils {
     public static rpcs: {[chainId: number]: string}={
-        1:"https://go.getblock.io/8e30f8a3d9314d4785167d79abdf164c",
-        56:"https://go.getblock.io/119fad79ee5d4ffda4c3de8fdc77618e",
-        137:"https://go.getblock.io/5fa1b57ec3724b18944c5c10006d95ef"
+        1:"https://go.getblock.io/e290a2fbe2574872924a89852fa341f0",
+        56:"https://go.getblock.io/afb9702b784d4b2fa1b1b43290ecf787",
+        137:"https://go.getblock.io/64929c98232f46928335f57b7faeeb07"
     }
 
     public static getRpc(chainId: number): string {
