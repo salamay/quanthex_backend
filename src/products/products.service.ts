4b84791ab71071ab815a5ec8ceb96fecef838f83
Salamay <ayotundesalam16@gmail.com>
2026-02-15 01:51:21 +0100

    Corrected

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -531,9 +531,10 @@ export class ProductsService {
         this.logger.debug(`Subscription ID: ${subscriptionId}`)
         const referrals: ReferralDto[] = []
         const query = `SELECT * FROM referrals r
+                       LEFT JOIN profiles p ON r.referral_ancestor_uid = p.uid 
                        WHERE r.referral_ancestor_uid = ? AND r.referral_ancestor_sub_id = ?`;
         const referralRepository = this.dataSource.manager.getRepository(ReferralEntity)
-        const results: [] = await referralRepository.query(query, [uid, ,subscriptionId])
+        const results: [] = await referralRepository.query(query, [uid, subscriptionId])
         this.logger.debug(`Found ${results.length} indirect referrals for user ${uid}`)
         for (const row of results) {
             const referralDto = new ReferralDto()
